c1ae640c3726d90ddf6f76a821678cd4ab5cf9ed
Alex Egorov <alex-egorov-01@list.ru>
2021-11-01 15:16:50 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,110 @@
+import os
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, Field, EmailStr
+from bson import ObjectId
+from typing import Optional, List
+import motor.motor_asyncio
+
+app = FastAPI()
+MONGODB_URL = "mongodb+srv://admin:123@cluster0.m72k6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
+db = client.our_date
+
+# Настройки для базы
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class StudentModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    email: EmailStr = Field(...)
+    course: str = Field(...)
+    gpa: float = Field(..., le=4.0)
+
+
+class UpdateStudentModel(BaseModel):
+    name: Optional[str]
+    email: Optional[EmailStr]
+    course: Optional[str]
+    gpa: Optional[float]
+
+
+
+# Добавить студента 
+@app.post("/", response_description="Add new student", response_model=StudentModel)
+async def create_student(student: StudentModel = Body(...)):
+    student = jsonable_encoder(student)
+    new_student = await db["students"].insert_one(student)
+    created_student = await db["students"].find_one({"_id": new_student.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
+
+
+
+# Получить список студентов
+@app.get(
+    "/", response_description="List all students", response_model=List[StudentModel]
+)
+async def list_students():
+    students = await db["students"].find().to_list(1000)
+    return students
+
+
+
+# Отдельный чел 
+@app.get(
+    "/{id}", response_description="Get a single student", response_model=StudentModel
+)
+async def show_student(id: str):
+    if (student := await db["students"].find_one({"_id": id})) is not None:
+        return student
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+
+
+
+# Обновить челика
+@app.put("/{id}", response_description="Update a student", response_model=StudentModel)
+async def update_student(id: str, student: UpdateStudentModel = Body(...)):
+    student = {k: v for k, v in student.dict().items() if v is not None}
+
+    if len(student) >= 1:
+        update_result = await db["students"].update_one({"_id": id}, {"$set": student})
+
+        if update_result.modified_count == 1:
+            if (
+                updated_student := await db["students"].find_one({"_id": id})
+            ) is not None:
+                return updated_student
+
+    if (existing_student := await db["students"].find_one({"_id": id})) is not None:
+        return existing_student
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+
+
+
+# Удалить челика
+@app.delete("/{id}", response_description="Delete a student")
+async def delete_student(id: str):
+    delete_result = await db["students"].delete_one({"_id": id})
+
+    if delete_result.deleted_count == 1:
+        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found")
